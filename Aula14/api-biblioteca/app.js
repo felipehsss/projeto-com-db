dfe71853e6ce6f57d65c062b4a12d9851f40f255
felipehsss <felipehsacchi@gmail.com>
2025-05-06 17:21:20 -0300

    segundo commit

--- Aula14/api-biblioteca/app.js
+++ Aula14/api-biblioteca/app.js
@@ -3,11 +3,26 @@ const app = express();
 const port = 3000;
 
 import livroRotas from './routes/livroRotas.js'
-app.use('/livros',livroRotas)
+app.use('/livros', livroRotas)
 
-app.get('/', (req,res) =>{
+
+app.get('/', (req, res) => {
   res.status(200).send('API de Biblioteca');
 })
+
+app.options('/', (req, res) => {
+  res.setHeader('Allow', 'GET, OPTIONS');
+  res.status(204).send();
+})
+app.options('/:id', (req, res) => {
+  res.setHeader('Allow', 'GET, OPTIONS');
+  res.status(204).send();
+})
+
+app.use((req, res) => {
+  res.status(404).json({ mensagem: 'Rota não encontrada' });
+})
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port} `)
